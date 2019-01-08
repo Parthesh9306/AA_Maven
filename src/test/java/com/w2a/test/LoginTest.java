41dc749d699c5e313c8008775d2e31f011c7a3ac
partheshjoshi <parthesh9306@gmail.com>
2019-01-08 20:20:52 +0530

    First commit of maven project

---
+++ src/test/java/com/w2a/test/LoginTest.java
@@ -0,0 +1,35 @@
+package com.w2a.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+	WebDriver driver;
+	
+	@BeforeSuite
+	public void setup()
+	{
+		String chromedriverpath = "D:\\Parthesh IMP\\Selenium\\chromedriver_win32\\chromedriver.exe";
+		System.setProperty("webdriver.chrome.driver", chromedriverpath);
+		driver = new ChromeDriver();
+		
+	}
+	
+	@Test
+	public void doLogin()
+	{
+		driver.get("https://www.freecrm.com/index.html");
+	}
+	
+	@AfterSuite
+	public void quit()
+	{
+		driver.quit();
+	}
+	
+	
+}
